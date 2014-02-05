9e9f3c942103819c3ca37cbed79f0496a4aa908e
tnk <mercuryg4@gmail.com>
2014-02-06 00:42:27 +0900

    first commit

---
+++ rollover_css.js
@@ -0,0 +1,57 @@
+/*!
+ * jQuery rollover css
+ * Original author: @bluebox
+ * Licensed under the MIT license
+ * version : 0.1
+ */
+
+(function($) {
+
+    $.fn.rollover = function(options) {
+
+        var settings = $.extend({
+            opacity: 0,
+            duration: 300,
+            over: '_hover'
+        }, options);
+
+        this.each(function() {
+            var img = jQuery(this).find('img'),
+                src = img.attr('src'),
+                reg = new RegExp(settings.over),
+                // imgH = img.height(),
+                // imgW = img.width();
+
+            if (src.match(reg)) return;
+
+            console.log(img, src, imgH, imgW)
+            var _hover = src.replace(/\.\w+$/, settings.over + '$&');
+            $(this).css({
+                'background': 'url(' + _hover + ') no-repeat 0 0',
+                // 'width': imgW,
+                // 'height': imgH
+            }).hover(
+                function() {
+                    // $(this).find('img').stop(true).fadeTo(settings.duration, settings.opacity);
+                    $(this).find('img').stop(true).animate({
+                        'opacity': settings.opacity
+                    }, settings.duration);
+                },
+                function() {
+                    // $(this).find('img').stop(true).fadeTo(settings.duration, 1);
+                    $(this).find('img').stop(true).animate({
+                        'opacity': '1'
+                    }, settings.duration);
+                }
+            );
+        });
+
+        return this;
+    };
+
+}(jQuery));
+
+$('nav li a').rollover({
+    opacity: 0,
+    duration: 700
+});
